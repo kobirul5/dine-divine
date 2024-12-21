3a7d5fc129d4db4be8801adde9873db3c35aaa92
kobirul5 <kobirul05j@gmail.com>
2024-12-21 20:57:28 +0600

    added navbar section and make it diynamic based on user data

--- src/pages/Share/Navbar.jsx
+++ src/pages/Share/Navbar.jsx
@@ -34,26 +34,28 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end gap-3">
-                <a className="btn">Log Out</a>
-                <div className="dropdown dropdown-end">
+                
+                {
+                    user?.email ? 
+                    <button className="btn">Log Out</button>
+                    :
+                    <button className="btn">LogIn</button>
+                }
+                {/* dropdown profile option */}
+                <div className={`dropdown dropdown-end ${user?.email ? "flex": "hidden"}`}>
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <img
                                 alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                src={user?.photoURL} />
                         </div>
                     </div>
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
+                        <li><a>My Foods</a></li>
+                        <li><a>Add food</a></li>
+                        <li><a>My Orders</a></li>
                     </ul>
                 </div>
             </div >
